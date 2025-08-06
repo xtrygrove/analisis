b49891db01e604ba2f78e0aaa16f67348b1fc5a5
xtrygroove <jefuentes1@email.com>
2025-08-06 18:16:52 -0400

    cambio parametros

--- app_streamlit_spx_10y_vix.py
+++ app_streamlit_spx_10y_vix.py
@@ -22,14 +22,14 @@ st.title('Indicadores cuantitativos')
 
 # --- Parámetros Fijos ---
 # Tickers
-tickers = ['^GSPC', '^TNX', '^VIX'] # '^GSPC', '^TNX', '^VIX'
+tickers = ['^GSPC', '^TNX', '^VIX']
 ticker_names = {'^GSPC': 'SPX', '^TNX': 'TNX', '^VIX': 'VIX'}
 
 # Ventana para el cálculo móvil (rolling)
-rolling_window_beta = 33 # 4, 13, 33
+rolling_window_beta = 13 # 4, 13, 33
 rolling_window_vol, long_window_vol = 13, 60 # For RV and Vol of Vol / For 3M RV
 trading_days_per_year = 252 # For annualizing volatility
-period = '2y'    # 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max 
+period = '1y'    # 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max 
 interval = '1d'  # 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
 
 data = pd.DataFrame()
