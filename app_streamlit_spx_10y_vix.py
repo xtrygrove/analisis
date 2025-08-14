502fc05a1d2acbd80320f78925dded681819dc9a
xtrygroove <jefuentes1@gmail.com>
2025-08-13 22:47:16 -0400

    1 año

--- app_streamlit_spx_10y_vix.py
+++ app_streamlit_spx_10y_vix.py
@@ -29,7 +29,7 @@ ticker_names = {'^GSPC':'SPX', '^TNX':'TNX', '^VIX':'VIX'}
 rolling_window_beta = 17 # 4, 13, 33
 rolling_window_vol, long_window_vol = 21, 63 # For RV and Vol of Vol / For 3M RV
 trading_days_per_year = 252 # For annualizing volatility
-period = '6mo'           # 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max 
+period = '1y'           # 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max 
 interval = '1d'         # 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
 
 # data = pd.DataFrame()
